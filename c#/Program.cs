baf10903ffd9db3ad582ca50baf3245e7d83fe4c
monoxide <numpad0000@gmail.com>
2024-10-19 00:14:48 +0100

    Add files via upload

---
+++ c#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("don't type 'ksi i love you' ");
+        string HereWeGo = Console.ReadLine();
+
+        if (HereWeGo.Equals("ksi i love you", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("I'm in the thick of it, everybody knows\nThey know me where it snows, I skied in and they froze\nI don't know no nothin' 'bout no ice, I'm just cold\nForty somethin' milli' subs or so, I've been told\n\nI'm in my prime, and this ain't even final form\nThey knocked me down, but still, my feet, they find the floor\nI went from living rooms straight out to sold out tours\nLife's a fight, but trust, I'm ready for the war\n\nWoah-oh-oh\nThis is how the story goes\nWoah-oh-oh\nI guess this is how the story goes\n\nI'm in the thick of it, everybody knows\nThey know me where it snows, I skied in and they froze\nI don't know no nothin' 'bout no ice, I'm just cold\nForty somethin' milli' subs or so, I've been told\n\nFrom the screen to the ring, to the pen, to the king\nWhere's my crown? That's my bling\nAlways drama when I ring\nSee, I believe that if I see it in my heart\nSmash through the ceiling 'cause I'm reaching for the stars\n\nWoah-oh-oh\nThis is how the story goes\nWoah-oh-oh\nI guess this is how the story goes\n\nI'm in the thick of it, everybody knows\nThey know me where it snows, I skied in and they froze (Woo)\nI don't know no nothin' 'bout no ice, I'm just cold\nForty somethin' milli' subs or so, I've been told\n\nHighway to heaven, I'm just cruisin' by my lone'\nThey cast me out, left me for dead, them people cold\nMy faith in God, mind in the sun, I'm 'bout to sow (Yeah)\nMy life is hard, I took the wheel, I cracked the code (Yeah-yeah, woah-oh-oh)\nAin't nobody gon' save you, man, this life will break you (Yeah, woah-oh-oh)\nIn the thick of it, this is how the story goes\n\nI'm in the thick of it, everybody knows\nThey know me where it snows, I skied in and they froze\nI don't know no nothin' 'bout no ice, I'm just cold\nForty somethin' milli' subs or so, I've been told\nI'm in the thick of it, everybody knows (Everybody knows)\nThey know me where it snows, I skied in and they froze (Yeah)\nI don't know no nothin' 'bout no ice, I'm just cold\nForty somethin' milli' subs or so, I've been told (Ooh-ooh)\n\nWoah-oh-oh (Nah-nah-nah-nah, ayy, ayy)\nThis is how the story goes (Nah, nah)\nWoah-oh-oh\nI guess this is how the story goes ");
+        }
+       
+        else
+        {
+            Console.WriteLine("you got lucky");
+           
+        }
+    }
+}
