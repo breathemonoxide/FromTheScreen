9e1254f143ce16cffd0340e00a8e4c08a680651c
monoxide <numpad0000@gmail.com>
2024-10-19 00:44:53 +0100

    Update FromTheScreen C++.cpp

--- c++/FromTheScreen C++.cpp
+++ c++/FromTheScreen C++.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
-#include <algorithm>  // For std::transform
+#include <algorithm>  
 
 int main() {
     std::string userInput;
 
-    // Ask the user for a phrase
+   
     std::cout << "don't say 'ksi i love you' ";
     std::getline(std::cin, userInput);
 
-    // Convert input to lowercase for case-insensitive comparison
+    
     std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::tolower);
 
-    // Check for specific phrases and respond accordingly
+
     if (userInput == "ksi i love you") {
         std::cout << "You really thought huh? why would i let you see these lyrics?? " << std::endl;
     }
