3102a5d285d6ff0c48e5c7e48fc72b1af74811bf
monoxide <numpad0000@gmail.com>
2024-10-19 00:53:41 +0100

    Add files via upload

---
+++ F#/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("don't say 'ksi i love you' ")
+    let userInput = Console.ReadLine().ToLower()
+
+  
+    match userInput with
+    | "hello" -> Console.WriteLine("So basically F# did not want to let me paste the lyrics and its 12:51AM so no thank you :)")
+    | _ ->
+        Console.WriteLine("you got lucky")
+
+    0 
