4926851175ef3d7df528bdb679a20600038b1aef
monoxide <numpad0000@gmail.com>
2024-10-19 10:02:08 +0100

    Update Program.fs

--- F#/Program.fs
+++ F#/Program.fs
@@ -7,7 +7,7 @@ let main argv =
 
   
     match userInput with
-    | "hello" -> Console.WriteLine("So basically F# did not want to let me paste the lyrics and its 12:51AM so no thank you :)")
+    | "ksi i love you" -> Console.WriteLine("So basically F# did not want to let me paste the lyrics and its 12:51AM so no thank you :)")
     | _ ->
         Console.WriteLine("you got lucky")
 
